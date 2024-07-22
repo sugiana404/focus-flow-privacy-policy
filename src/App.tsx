4f919dc0d4b6e2ab034f87f5c44a05e3695badef
sugiana404 <sugiana0404@outlook.com>
2024-07-22 17:06:10 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="bg-gray-950 text-gray-100 text-xl px-32 py-16 text-justify">
+      <h1 className="font-bold text-3xl pb-4">Privacy Policy</h1>
+      <h1 className="pl-4 pb-4">Last updated: 22/07/2024</h1>
+      <h1 className="font-bold text-3xl pb-4">Introduction</h1>
+      <h2 className="font-normal text-s pl-4 text-neutral-300 pb-4">
+        Welcome to FocusFlow! FocusFlow is commited to protecting your privacy.
+        This Privacy Policy explains how we handle your personal information
+        when you use our mobile application.
+      </h2>
+      <h1 className="font-bold text-3xl pb-4">Data Collection and Use</h1>
+      <h2 className="font-normal text-s pl-4 text-neutral-300 pb-4">
+        Our mobile application does not collect, store, or share any personal
+        data or user information. All data is stored locally on your device and
+        is not transmitted to any external servers or third parties. The app
+        functions independently without accessing the internet for data
+        processing.
+      </h2>
+      <h1 className="font-bold text-3xl pb-4">Local Storage</h1>
+      <h2 className="font-normal text-s pl-4 text-neutral-300 pb-4">
+        The app uses a local database to store all necessary information for its
+        operations. This data is not accessible by us or any third party and
+        remains entirely within your controle.
+      </h2>
+      <h1 className="font-bold text-3xl pb-4">Security</h1>
+      <h2 className="font-normal text-s pl-4 text-neutral-300 pb-4">
+        We prioritize your data security. The app has been designed to operate
+        offline and does not send or recieve data over the internet, minimizing
+        any risk of data breaches.
+      </h2>
+      <h1 className="font-bold text-3xl pb-4">Your Rights</h1>
+      <h2 className="font-normal text-s pl-4 text-neutral-300 pb-4">
+        Since we do not collect any personal data, there are no rights related
+        to data access, correction, or deletion applicable to our app.
+      </h2>
+      <h1 className="font-bold text-3xl pb-4">
+        Changes to This Privacy Policy
+      </h1>
+      <h2 className="font-normal text-s pl-4 text-neutral-300 pb-4">
+        We may update our Privacy Policy from time to time. We will notify you
+        of any changes by posting the new Privacy Policy on this page. You are
+        advised to review this privacy.
+      </h2>
+    </div>
+  );
+}
+
+export default App;
